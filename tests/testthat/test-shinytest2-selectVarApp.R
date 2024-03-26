2f5bcf7017fd79bdbf7b7d1e40d869151c54e0fc
Martin Frigaard <mjfrigaard@pm.me>
2024-03-26 13:51:41 -0700

    updates to tests

--- tests/testthat/test-shinytest2-selectVarApp.R
+++ tests/testthat/test-shinytest2-selectVarApp.R
@@ -1,21 +1,21 @@
 library(shinytest2)
-remotes::install_github("rstudio/chromote", force = TRUE, quiet = TRUE)
+# remotes::install_github("rstudio/chromote", force = TRUE, quiet = TRUE)
 library(chromote)
 
 test_that("{shinytest2}: selectVarApp", {
   app <- AppDriver$new(app_dir = system.file("dev", "selectVarApp",
                                              package = "mstsap"),
                        height = 600, width = 600)
+  # app$view()
   app$set_inputs(`data-dataset` = "mtcars")
   app$set_inputs(`var-var` = "wt")
 
   app_values <- app$get_values()
 
-  testthat::expect_true(
+  expect_true(
     object = is.data.frame(app_values$export$data))
-
-  testthat::expect_true(
+  expect_true(
     object = ncol(app_values$export$var) == 1)
-
+  
   app$stop()
 })
