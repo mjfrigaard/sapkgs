10c9b77388903836cf775337e237afe2cb8a89c5
Martin Frigaard <mjfrigaard@pm.me>
2024-04-03 10:50:17 -0700

    updates to rhino app

--- app/main.R
+++ app/main.R
@@ -1,13 +1,17 @@
 # app/main.R
 
-# shiny functions
 box::use(
+  # shiny functions
   shiny[
     NS, fluidPage, sidebarLayout, sidebarPanel, 
     mainPanel, fluidRow, column, tags, icon,
     plotOutput, moduleServer, renderPlot
   ],
-  shinythemes[shinytheme]
+  # bslib functions
+  bslib[
+    layout_sidebar, bs_theme, sidebar, card, 
+    card_body, card_title, card_header
+  ]
 )
 
 # import modules ----
@@ -26,40 +30,43 @@ box::use(
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  fluidPage(theme = shinytheme("spacelab"),
-    sidebarLayout(
-      sidebarPanel(
+  layout_sidebar(theme = bs_theme(
+      # Controls the default grayscale palette
+      bg = "#202123", 
+      fg = "#B8BCC2",
+      # Controls the accent (e.g., hyperlink, button, etc) colors
+      primary = "#EA80FC", 
+      secondary = "#48DAC6",
+      base_font = c("Grandstander", "sans-serif"),
+      code_font = c("Hack", "monospace"),
+      heading_font = "'Helvetica Neue', Helvetica, sans-serif",
+      # Can also add lower-level customization
+      "input-border-color" = "#EA80FC"),
+    sidebar = sidebar(
         # use inputs module UI 
         inputs$ui(ns("vals"))
         ),
-      mainPanel(
-        fluidRow(
-              # use clicks module UI 
-              clicks$ui(ns("clicks")),
-              # use message module UI 
-              message$ui(ns("message"))
-          ),
-        fluidRow(
-          column(
-            width = 1,
-            offset = 11,
-            # example info button 
-            tags$button(id = "help-button",
-              icon("info"),
-              # add 'onclick' after rhino::build_sass()
-              # and rhino::build_js()
-              onclick = "App.showHelp()")
+    card(
+      card_header(
+          tags$em(
+            "These data were obtained from",
+            tags$a("IMBD", href = "http://www.imbd.com/"), "and",
+            tags$a("Rotten Tomatoes", href = "https://www.rottentomatoes.com/"),
+            ". The data represent 651 randomly sampled movies released between
+            1972 to 2014 in the United States."
           )
         ),
-        fluidRow(
-            column(
-              width = 12,
-              tags$h3("rtapp"),
-              # use display module UI 
-            display$ui(ns("disp"))
-            )
-          )
-        )
+        # use clicks module UI
+        # clicks$ui(ns("clicks")),
+        # use message module UI
+        # message$ui(ns("message")),
+        # example info button
+        tags$button(id = "help-button", icon("info"),
+          # add 'onclick' after rhino::build_sass()
+          # and rhino::build_js()
+          onclick = "App.showHelp()"),
+        # use display module UI 
+        display$ui(ns("disp"))
       )
     )
 }
@@ -69,9 +76,9 @@ ui <- function(id) {
 server <- function(id) {
   
   moduleServer(id, function(input, output, session) {
-    # use inputs module server 
+    # use inputs module server
     selected_vars <- inputs$server(id = "vals")
-    # use display module server 
+    # use display module server
     display$server(
       id = "disp",
       var_inputs = selected_vars
@@ -79,8 +86,8 @@ server <- function(id) {
   })
   
   moduleServer(id, function(input, output, session) {
-    clicks$server("clicks")
-    message$server("message")
+    # clicks$server("clicks")
+    # message$server("message")
   })
   
 }
--- app/view/display.R
+++ app/view/display.R
@@ -13,31 +13,18 @@ ui <- function(id) {
     shiny[NS, tagList, tags, plotOutput]
   )
   ns <- NS(id)
-  # use data$movies_data() ----
   tagList(
-    tags$br(),
-    tags$p(
-      "These data were obtained from",
-      tags$a("IMBD", href = "http://www.imbd.com/"), "and",
-      tags$a("Rotten Tomatoes", href = "https://www.rottentomatoes.com/"), 
-      ". The data represent 651 randomly sampled movies released between 
-      1972 to 2014 in the United States."
-    ),
-    tags$hr(),
+    # use data$movies_data() ----
     plotOutput(outputId = ns("scatterplot")),
-    tags$hr(),
-    tags$blockquote(
+    tags$br(),
       tags$em(
-        tags$h6(
           "The code for this application comes from the ",
           tags$a("Building web applications with Shiny",
             href = "https://rstudio-education.github.io/shiny-course/"
           ),
           "tutorial"
         )
-      )
     )
-  )
 }
 
 #' display server
@@ -47,7 +34,7 @@ server <- function(id, var_inputs) {
   # load plotting, shiny, tools, and stringr functions
   box::use(
     ggplot2[labs, theme_minimal, theme],
-    shiny[NS, moduleServer, plotOutput, reactive, renderPlot],
+    shiny[NS, req, moduleServer, plotOutput, reactive, renderPlot],
     tools[toTitleCase],
     stringr[str_replace_all]
   )
@@ -70,6 +57,7 @@ server <- function(id, var_inputs) {
     })
 
     output$scatterplot <- renderPlot({
+      req(c(inputs(), movies))
       # use plot$scatter_plot() ----
       plot <- plot$scatter_plot(
         df = movies,
@@ -90,5 +78,6 @@ server <- function(id, var_inputs) {
         theme_minimal() +
         theme(legend.position = "bottom")
     })
+    
   })
 }
